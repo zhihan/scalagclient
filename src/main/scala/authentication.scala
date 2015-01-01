64e44153876a8782fa842e5b85b11a0de56e6584
zhihan <zhi.han@gmail.com>
2014-12-31 22:49:36 -0500

    Added oauth2 and retrieve userInfo

--- src/main/scala/authentication.scala
+++ src/main/scala/authentication.scala
@@ -17,6 +17,8 @@ import com.google.api.services.oauth2.Oauth2
 object Constants {
   val CLIENT_ID = "1022278465531-4251eu09npkop9ip88l3eov0us7cfkei.apps.googleusercontent.com"
   val CLIENT_SECRET = "6Jcsab2XoW_p0jxuG6DwCV8L"
+
+  val APPLICATION_NAME = "scala google client"
 }
 
 object Auth {
@@ -25,14 +27,25 @@ object Auth {
 
   private val scopes = List("https://www.googleapis.com/auth/userinfo.email")
 
-
-  def authorize(user:String): Credential = {
+  /** 
+    Authorize the user using the installed application flow and return
+    oauth2 interface object.
+  */
+  def authorize(user:String): Oauth2 = {
     val flow = new GoogleAuthorizationCodeFlow.Builder(
       httpTransport, jsonFactory,
       Constants.CLIENT_ID, Constants.CLIENT_SECRET,
       scopes.toIterable).build()
 
-    new AuthorizationCodeInstalledApp(
+    val credential = new AuthorizationCodeInstalledApp(
       flow, new LocalServerReceiver()).authorize(user);
+
+    new Oauth2.Builder(httpTransport, jsonFactory,
+      credential).setApplicationName(Constants.APPLICATION_NAME).build()
   }
+    
+  /** Getting the user info from an existing OAuth2 client */
+  def userInfo(oauth2: Oauth2) = 
+    oauth2.userinfo().get().execute()
+  
 }
